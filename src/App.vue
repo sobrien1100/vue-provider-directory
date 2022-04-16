5ea18d7aba89bdc013e5920df126adf92c407ba6
Sean O'Brien <sobrien1100gmail.com>
2022-04-16 09:12:18 -0400

    Working sort functionality

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,153 @@
 <template>
-<div class=container>
-  <Header @toggle-add-provider="toggleAddProvider" title="Provider Directory" :showAddProvider="showAddProvider"/> 
-  <AddProvider v-show="showAddProvider" @add-provider="addProvider" />
-  <SortFilter @provider-search="providerSearch" />
-  <Providers @delete-provider="deleteProvider" :providers="providers" />
-  
-</div>
+  <div class="container">
+    <Header
+      @toggle-add-provider="toggleAddProvider"
+      title="Provider Directory"
+      :showAddProvider="showAddProvider"
+    />
+    <AddProvider v-show="showAddProvider" @add-provider="addProvider" />
+    <SortFilter
+      @provider-search="providerSearch"
+      @provider-sort="providerSort"
+    />
+    <Providers @delete-provider="deleteProvider" :providers="providers" />
+  </div>
 </template>
 
 <script>
-import Header from './components/Header'
-import Providers from './components/Providers'
-import AddProvider from './components/AddProvider'
-import SortFilter from './components/SortFilter'
-import MOCK_DATA from './MOCK_DATA.json'
-
+import Header from "./components/Header";
+import Providers from "./components/Providers";
+import AddProvider from "./components/AddProvider";
+import SortFilter from "./components/SortFilter";
+import MOCK_DATA from "./MOCK_DATA.json";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Header, 
-    Providers, 
-    AddProvider, 
-    SortFilter
-  }, 
+    Header,
+    Providers,
+    AddProvider,
+    SortFilter,
+  },
   data() {
     return {
       providers: MOCK_DATA,
-      filterProviders: [], 
-      showAddProvider: false, 
-      filterText: null, 
-    }
+      filterProviders: [],
+      showAddProvider: false,
+      filterText: null,
+      sortDirection: null,
+    };
   },
   methods: {
-    providerSearch(filterText) {
-      this.providers = MOCK_DATA
-      this.providers = this.providers.filter((provider) => {
-        return provider.first_name.toLowerCase().includes(filterText.toLowerCase())
-      })
-
-      console.log("providers after first name search", this.providers)
-
-      if(this.providers.length === 0) {
-        this.providers = MOCK_DATA
-        this.providers = this.providers.filter((provider) => {
-        return provider.last_name.toLowerCase().includes(filterText.toLowerCase())
-        })
+    providerSort(sortText) {
+      if (this.sortField === sortText) {
+        switch (this.sortDirection) {
+          case null:
+            this.sortDirection = "asc";
+            break;
+          case "asc":
+            this.sortDirection = "desc";
+            break;
+          case "desc":
+            this.sortDirection = "asc";
+            break;
+        }
+      } else {
+        this.sortDirection = "asc";
       }
 
-      console.log("Providers log After", this.providers)
+      console.log('this.sortDirection' , this.sortDirection)
+
+      switch (sortText) {
+        case "first":
+          this.sortField = "first";
+          this.providers.sort((a, b) => {
+            const sign =
+              a.first_name.toLowerCase() > b.first_name.toLowerCase() ? 1 : -1;
+            return this.sortDirection === "asc" ? sign : -sign;
+          });
+          break;
+        case "last":
+          this.sortField = "last";
+          this.providers.sort((a, b) => {
+            const sign =
+            a.last_name.toLowerCase() > b.last_name.toLowerCase() ? 1 : -1;
+            return this.sortDirection === "asc" ? sign : -sign;
+          });
+          break;
+        case "email":
+          this.sortField = "email";
+          this.providers.sort((a, b) => {
+            const sign =
+            a.email_address.toLowerCase() > b.email_address.toLowerCase() ? 1 : -1; 
+            return this.sortDirection === "asc" ? sign : -sign;
+          });
+          break;
+        case "specialty":
+          this.sortField = "specialty";
+          this.providers.sort((a, b) => { 
+            const sign =
+            a.specialty.toLowerCase() > b.specialty.toLowerCase() ? 1 : -1;
+            return this.sortDirection === "asc" ? sign : -sign;
+          });
+          break;
+        case "practice":
+          this.sortField = "practice";
+          this.providers.sort((a, b) => {
+            const sign =
+            a.practice_name.toLowerCase() > b.practice_name.toLowerCase()? 1 : -1;
+            return this.sortDirection === "asc" ? sign : -sign;
+          });
+          break;
+      }
+    },
+    providerSearch(filterText) {
+      this.providers = MOCK_DATA;
+      this.providers = this.providers.filter((provider) => {
+        return provider.first_name
+          .toLowerCase()
+          .includes(filterText.toLowerCase());
+      });
+
+      console.log("providers after first name search", this.providers);
+
+      if (this.providers.length === 0) {
+        this.providers = MOCK_DATA;
+        this.providers = this.providers.filter((provider) => {
+          return provider.last_name
+            .toLowerCase()
+            .includes(filterText.toLowerCase());
+        });
+      }
+
+      console.log("Providers log After", this.providers);
     },
     toggleAddProvider() {
-      this.showAddProvider = !this.showAddProvider
+      this.showAddProvider = !this.showAddProvider;
     },
     addProvider(provider) {
-      this.providers = [...this.providers, provider]
-    }, 
+      this.providers = [...this.providers, provider];
+    },
     deleteProvider(email_address) {
-      console.log('provider', email_address)
-      this.providers = this.providers.filter((provider) => provider.email_address != email_address )
-    }
-  }, 
-}
+      console.log("delete provider", email_address);
+      if (confirm("Are you sure?")) {
+        this.providers = this.providers.filter(
+          (provider) => provider.email_address != email_address
+        );
+      }
+    },
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 body {
-  font-family: 'Poppins', sans-serif;
+  font-family: sans-serif;
 }
 .container {
   max-width: 50%;
--- src/components/SortFilter.vue
+++ src/components/SortFilter.vue
@@ -1,31 +1,72 @@
 <template>
-        <h3>Search By Name</h3>
-        <div class="form-control">
-        <input 
-            type="text" 
-            v-model="filterText" 
-            placeholder="Enter Search Text"
-            @input="onSearch(filterText)" />
-        </div>
+  <div class="form-control">
+    <div>
+      <h3>Search By Name</h3>
+      <input
+        type="text"
+        v-model="filterText"
+        placeholder="Enter Search Text"
+        @input="onSearch(filterText)"
+      />
+    </div>
+    <div>
+        <button @click="onClick('first')" :style="{background: color}" class="btn2">Sort By First Name</button>
+        <button @click="onClick('last')" :style="{background: color}" class="btn2">Sort By Last Name</button>
+        <button @click="onClick('email')" :style="{background: color}" class="btn2">Sort By Email</button>
+        <button @click="onClick('specialty')" :style="{background: color}" class="btn2">Sort By Specialty</button>
+        <button @click="onClick('practice')" :style="{background: color}" class="btn2">Sort By Practice Name</button>
+
+ 
+    </div>
+  </div>
 </template>
 
 
 <script>
 export default {
-  name: 'SortFilter',
+  name: "SortFilter",
   data() {
     return {
-      filterText: '',      
-    }
+      filterText: "",
+      sortField: ""
+    };
   },
   methods: {
-    onSearch(e) {     
-        console.log(e) 
-        // this.filterText = e.data;
-        console.log(this.filterText)
-      
-        this.$emit('provider-search', e)
+    onSearch(e) {
+      console.log(e);
+      // this.filterText = e.data;
+      console.log(this.filterText);
+
+      this.$emit("provider-search", e);
     },
+    onClick(e) {
+        this.$emit("provider-sort", e);
+    }
   },
-}
+};
 </script>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+h3 {
+  padding-right: 5px;
+}
+.btn2 {
+  padding: 5px 10px;
+  margin: 2px;
+  border-radius: 2px;  
+  font-size: 10px;
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  text-decoration: none;  
+  font-family: inherit;
+}
+</style>
